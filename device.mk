50da41da379e560a11138dfab226b48c126237ba
bengris32 <bengris32@protonmail.ch>
2023-10-01 23:35:48 +0100

    RMX3686: Configure Neural Networks stack

--- device.mk
+++ device.mk
@@ -143,6 +143,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor \
+    libtextclassifier_hash.vendor
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
