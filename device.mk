808d00a3d3b9a4b0ce94500d75a6c5b419c3a2e0
Michael Bestas <mkbestas@lineageos.org>
2024-01-16 21:45:06 +0000

    RMX3686: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- device.mk
+++ device.mk
@@ -157,6 +157,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.oplus
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail_vendor \
